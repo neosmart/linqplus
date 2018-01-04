96fbe0571ae39cc52ba9a95f915927f863338cf4
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-01-04 13:34:16 -0600

    Implement .Is<T>(), .IsNot<T>(), and .As<T>() for struct and class
    There are several commented-out implementations of .As<T> for different
    approaches that take into account value type vs ref type vs nullable
    value type; but the currently selected implementation provides a better
    mix of compile-time error reporting where possible.

--- LinqPlus/ExtendedLinq.cs
+++ LinqPlus/ExtendedLinq.cs
@@ -6,6 +6,14 @@ namespace NeoSmart.Linq
 {
     public static class ExtendedLinq
     {
+        public abstract class IClass<T> where T : class
+        {
+        }
+
+        public abstract class IStruct<T> where T : struct
+        {
+        }
+
         public static bool Contains<T>(this IEnumerable<T> container, Func<T, bool> where)
         {
             return container.Where(where).Any();
@@ -18,7 +26,7 @@ namespace NeoSmart.Linq
 
         public static bool ContainsAnyOf<T>(this IEnumerable<T> container, SortedSet<T> values)
         {
-            foreach(var value in container)
+            foreach (var value in container)
             {
                 if (container.Contains(value))
                 {
@@ -31,7 +39,7 @@ namespace NeoSmart.Linq
 
         public static bool ContainsAllOf<T>(this IEnumerable<T> container, IEnumerable<T> values)
         {
-            foreach(var value in values)
+            foreach (var value in values)
             {
                 if (!container.Contains(value))
                 {
@@ -60,6 +68,120 @@ namespace NeoSmart.Linq
         public static bool Empty<T>(this IEnumerable<T> container)
         {
             return !container.Any();
+        }
+
+        //The goal here is to act like `object as T` for classes (null on invalid conversion) but still
+        //allow the method to be used to avoid parentheses hell for structs, e.g. `if (((T)t).X)` can be
+        //written as simply `if (t.As<T>().X`.
+        //
+        //If C# allowed for overloading generic functions by constraints, we could split this
+        //into As<T> where T: class and As<T> where T: struct, with one return null and the other
+        //throwing an InvalidCastException; but we can't.
+        //public static T As<T>(this object @object)
+        //    where T: class
+        //{
+        //    if (@object == null)
+        //    {
+        //        return null;
+        //    }
+
+        //    try
+        //    {
+        //        return (T)@object;
+        //    }
+        //    catch (InvalidCastException)
+        //    {
+        //        Type type = @object.GetType();
+        //        if (type.GetTypeInfo().IsValueType)
+        //        {
+        //            //reference type
+        //            return null;
+        //        }
+        //        else if (Nullable.GetUnderlyingType(type) != null)
+        //        {
+        //            //Nullable<x>, also return null on invalid cast
+        //            return null;
+        //        }
+
+        //        throw;
+        //    }
+        //}
+
+#if false
+        //class.As<struct>: throw exception if not possible
+        public static Y As<Y,X>(this X x, IClass<X> _  = null, IStruct<Y> _y = null)
+            where X : class
+            where Y : struct
+        {
+            return (Y)(object)x;
+        }
+
+        //class.As<class>: return null if not possible
+        public static Y As<Y,X>(this X x, IClass<X> _ = null)
+            where X : class
+            where Y : class
+        {
+            return x as Y;
+        }
+
+        //struct.As<struct>: throw exception if not possible
+        public static Y As<Y,X>(this X x, IStruct<X> _ = null, IStruct<Y> _y = null)
+            where X : struct
+            where Y : struct
+        {
+            return (Y)(object)x;
+        }
+
+        //struct.As<class>: return null if not possible
+        public static Y As<Y, X>(this X x, IStruct<X> _ = null, IClass<Y> _y = null)
+            where X : struct
+            where Y : class
+        {
+            try
+            {
+                return (Y)(object)x;
+            }
+            catch (InvalidCastException)
+            {
+                return null;
+            }
+        }
+
+        //Nullable<struct>.As<struct>: throw exception if not possible
+        public static Y As<Y,X>(this X? x, IStruct<X> _ = null, IStruct<Y> _y = null)
+            where X : struct
+            where Y : struct
+        {
+            return (Y)(object)x;
+        }
+
+        //Nullable<struct>.As<class>: return null if not possible
+        public static Y As<Y, X>(this X? x, IStruct<X> _ = null, IClass<Y> _y = null)
+            where X : struct
+            where Y : class
+        {
+            try
+            {
+                return (Y)(object)x;
+            }
+            catch (InvalidCastException)
+            {
+                return null;
+            }
+        }
+#endif
+        //object.As<class>: return null if not possible
+        public static Y As<Y>(this object x, IClass<Y> _ = null)
+            where Y : class
+        {
+            return x as Y;
+        }
+
+        //object.As<struct>: throw exception if not possible
+        public static Y As<Y>(this object x, IStruct<Y> _ = null)
+            where Y : struct
+        {
+            return (Y)(object)x;
         }
 
         public static bool Is<T>(this object @object)
