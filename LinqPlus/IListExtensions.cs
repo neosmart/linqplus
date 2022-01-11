59de9ae31d45f0f79ba156574bdff5047585cb7a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-01-11 12:14:05 -0600

    Fix stop condition in IEnumerable<T>.Reversed()

--- LinqPlus/IListExtensions.cs
+++ LinqPlus/IListExtensions.cs
@@ -8,7 +8,7 @@ namespace NeoSmart.Linq
     {
         public static IEnumerable<T> Reversed<T>(this IList<T> list)
         {
-            for (int i = list.Count - 1; i <= 0; --i)
+            for (int i = list.Count - 1; i >= 0; --i)
             {
                 yield return list[i];
             }
